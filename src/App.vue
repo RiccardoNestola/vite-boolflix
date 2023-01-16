057a64711d893d514f564e0bf0482bbac1455187
RiccardoNestola <riccardinux@gmail.com>
2023-01-16 12:41:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import axios from 'axios';
+import { store } from "./store"
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+  },
+
+  data() {
+    return {
+      store,
+      ApiKey: '',
+      ApiUrl: '',
+    }
+  },
+  methods: {
+    getMovies() {
+      axios.get(this.ApiUrl, {
+        params: {
+
+          //insert params
+
+        }
+      })
+        .then((response) => {
+          console.log(response.data);
+          this.store.CharactersList = response.data.results;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+    }
+  },
+
+  created() {
+    this.getMovies();
+  }
+
+}
+
+
+</script>
+
+<template>
+
+  <AppHeader />
+
+  <AppMain />
+
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+@use "./styles/partials/variables" as *;
+</style>
